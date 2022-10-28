a899f1778cb00c69a80bfce4c318aefb3359e0d1
not.just.white <not.just.white@gmail.com>
2022-10-28 14:29:47 +0300

    ui 1.0

--- main.py
+++ main.py
@@ -1,16 +1,61 @@
-# This is a sample Python script.
+# dialog.py
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+"""Dialog-style application."""
+
+import sys
+# import array
+from PyQt6.QtWidgets import (
+    QApplication,
+    QDialog,
+    QFormLayout,
+    QLineEdit,
+    QPushButton,
+)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+counter = 1
+#inputFields = [[mainTextBar, mainButton]]
+
+class Window(QDialog):
+
+    def __init__(self):
+        super().__init__(parent=None)
+        self.setWindowTitle("QDialog")
+        mainButton = QPushButton("+")
+        mainTextBar = QLineEdit()
+        formLayout = QFormLayout()
+
+        mainTextBar.setObjectName("mainTextBar")
+
+        mainButton.setObjectName("mainButton")
+        mainTextBar.setFixedWidth(400)
+        mainButton.setFixedWidth(30)
+        formLayout.addRow(mainTextBar, mainButton)
+        mainButton.clicked.connect(lambda: self._createInputField(formLayout, counter))
+        launchButton = QPushButton("Launch all")
+        launchButton.show()
+        self.setLayout(formLayout)
+
+    def _createInputField(self, formLayout, counter):
+        if counter < 19:
+            textBar = QLineEdit()
+            textBar.setObjectName("textBar" + str(counter))
+            textBar.setText(textBar.objectName())
+            textBar.setFixedWidth(400)
+            button = QPushButton("-")
+            button.setFixedWidth(30)
+            button.clicked.connect(lambda: self._deleteInputField(textBar, button))
+            # inputFields.append([textBar, button])
+            formLayout.addRow(textBar, button)
+            counter += 1
+
+    def _deleteInputField(self, textBar, button):
+        textBar.deleteLater()
+        button.deleteLater()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    app = QApplication([])
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
