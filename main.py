1c9b04fe139e0f0c844eef731ade87a6ec9dbf93
not.just.white <not.just.white@gmail.com>
2022-10-30 22:25:55 +0200

    ui 2.0

--- main.py
+++ main.py
@@ -1,53 +1,94 @@
-# dialog.py
 
-"""Dialog-style application."""
-
+import itertools
 import sys
-# import array
-from PyQt6.QtWidgets import (
-    QApplication,
-    QDialog,
-    QFormLayout,
-    QLineEdit,
-    QPushButton,
-)
+from PyQt6.QtWidgets import *
+from PyQt6.QtGui import *
+from PyQt6.QtCore import Qt, QRect
+
+sys._excepthook = sys.excepthook
 
 
-counter = 1
-#inputFields = [[mainTextBar, mainButton]]
+def my_exception_hook(exctype, value, traceback):
+    # Print the error and traceback
+    print(exctype, value, traceback)
+    # Call the normal Exception hook after
+    sys._excepthook(exctype, value, traceback)
+    sys.exit(1)
 
+
+# Set the exception hook to our wrapping function
+sys.excepthook = my_exception_hook
+
+textInputWidth = 405
+buttonWidth = 30
+
+funButtonWidth = 70
+funButtonHeight = 35
 class Window(QDialog):
+    count = 0
 
     def __init__(self):
         super().__init__(parent=None)
-        self.setWindowTitle("QDialog")
-        mainButton = QPushButton("+")
-        mainTextBar = QLineEdit()
-        formLayout = QFormLayout()
+        self.setWindowTitle("One click app")
 
+        scrollBox = QScrollArea(self)
+        scrollBox.setGeometry(5, 40, 480, 425)
+        scrollBox.setWidgetResizable(True)
+
+        scrollAreaWidgetContents = QWidget()
+        scrollAreaWidgetContents.setGeometry(QRect(0, 0, 289, 219))
+
+        formLayout = QFormLayout(scrollAreaWidgetContents)
+
+        mainTextBar = QLineEdit(scrollAreaWidgetContents)
+
+        mainButton = QPushButton(scrollAreaWidgetContents)
         mainTextBar.setObjectName("mainTextBar")
-
         mainButton.setObjectName("mainButton")
-        mainTextBar.setFixedWidth(400)
-        mainButton.setFixedWidth(30)
+        mainButton.setText("+")
+        mainTextBar.setFixedWidth(textInputWidth)
+        mainButton.setFixedWidth(buttonWidth)
         formLayout.addRow(mainTextBar, mainButton)
-        mainButton.clicked.connect(lambda: self._createInputField(formLayout, counter))
-        launchButton = QPushButton("Launch all")
-        launchButton.show()
-        self.setLayout(formLayout)
+        mainButton.clicked.connect(lambda: self._createInputField(formLayout))
 
-    def _createInputField(self, formLayout, counter):
-        if counter < 19:
-            textBar = QLineEdit()
-            textBar.setObjectName("textBar" + str(counter))
-            textBar.setText(textBar.objectName())
-            textBar.setFixedWidth(400)
-            button = QPushButton("-")
-            button.setFixedWidth(30)
-            button.clicked.connect(lambda: self._deleteInputField(textBar, button))
-            # inputFields.append([textBar, button])
+        label = QLabel("Input file paths", self)
+        # label.setGeometry(0, 10, 210, 12)
+        label.move(0, 0)
+        label.setMargin(10)
+        label.setFont(QFont("Segoe ui", 15))
+
+        launchButton = QPushButton("Launch all", self)
+        launchButton.setGeometry(500, 430, funButtonWidth, funButtonHeight)
+        launchButton.show()
+
+        closeButton = QPushButton("Close all", self)
+        closeButton.setGeometry(590, 430, funButtonWidth, funButtonHeight)
+        closeButton.show()
+
+        saveButton = QPushButton("Save set", self)
+        saveButton.setGeometry(680, 430, funButtonWidth, funButtonHeight)
+        saveButton.show()
+
+        # createMainField(scrollBox)
+        scrollBox.setWidget(scrollAreaWidgetContents)
+        label.show()
+        # scrollBox.setLayout(formLayout)
+        # scrollBox.show()
+
+        # self.setLayout(formLayout)
+
+
+
+    def _createInputField(self, formLayout):
+        self.count += 1  # newId = next(iter(self.count))
+        textBar = QLineEdit()
+        button = QPushButton("-")
+        textBar.setObjectName("textBar" + str(self.count))  # self.newId
+        textBar.setFixedWidth(textInputWidth)
+        button.setFixedWidth(buttonWidth)
+        button.clicked.connect(lambda: self._deleteInputField(textBar, button))
+        if self.count < 20:
             formLayout.addRow(textBar, button)
-            counter += 1
 
     def _deleteInputField(self, textBar, button):
         textBar.deleteLater()
@@ -56,6 +97,18 @@ class Window(QDialog):
 
 if __name__ == "__main__":
     app = QApplication([])
+    fontId = QFontDatabase.addApplicationFont("SegoeUI-Light.ttf")
+    if fontId < 0:
+        print('font not loaded')
+
+    families = QFontDatabase.applicationFontFamilies(fontId)
+    if len(families) > 0:
+        font = QFont(families[0])
+
     window = Window()
+    window.setGeometry(300, 300, 760, 480)
     window.show()
-    sys.exit(app.exec())
+    try:
+        sys.exit(app.exec())
+    except:
+        print("Exiting")
